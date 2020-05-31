339bdcc9b657893e721609d67032c8b43ac167f5
7enTropy7 <menon.uk1998@gmail.com>
2020-06-01 02:34:27 +0530

    prime theory graph

---
+++ primes_num_theorem.py
@@ -0,0 +1,45 @@
+from scipy import optimize
+from sympy import *
+import numpy as np
+from matplotlib import pyplot as plt
+
+def primesLessThan_N(n):
+    p = 1
+    while(prime(p)<n):
+        p += 1
+    return p-1
+
+print('------------ #Primes < n ------------')
+
+n = 100
+
+num_primes = []
+for i in range(2,n+1):
+    num_primes.append(primesLessThan_N(i))
+
+x = np.arange(2,n+1)
+y = num_primes
+
+log_values = []
+for i in x:
+    log_values.append(i/np.log(i))
+
+print(x)
+print(y)
+print(log_values)
+
+def test_func(x, a, b):
+    return x/np.log(x*a+b) 
+
+param, param_cov = optimize.curve_fit(test_func, x, y)
+print('Curve Parameter (a,b) = ',param)
+ans = (param[0]*x+param[1]) 
+  
+
+plt.plot(x, y,'-', color ='blue', label ="Actual") 
+plt.plot(x, log_values, '-', color ='red', label ="x/ln(x)")
+plt.plot(x, ans, '-', color ='green', label ="Optimized")
+plt.xlabel('N')
+plt.ylabel('#Primes')
+plt.legend()
+plt.show()
