5945466115dbe284d1bd2cb126f9236fa5daa9e2
7enTropy7 <menon.uk1998@gmail.com>
2020-06-02 00:02:22 +0530

    subplots added

--- primes_num_theorem.py
+++ primes_num_theorem.py
@@ -11,35 +11,60 @@ def primesLessThan_N(n):
 
 print('------------ #Primes < n ------------')
 
-n = 1000
+n = 100
 
 num_primes = []
 for i in range(2,n+1):
     num_primes.append(primesLessThan_N(i))
 
 x = np.arange(2,n+1)
-y = num_primes
 
 log_values = []
 for i in x:
     log_values.append(i/np.log(i))
 
-print(x)
-print(y)
-print(log_values)
 
-def test_func(x, a, b):
+def test_func_num_primes(x, a, b):
     return x/np.log(x*a+b) 
 
-param, param_cov = optimize.curve_fit(test_func, x, y)
-print('Curve Parameter (a,b) = ',param)
+param, param_cov = optimize.curve_fit(test_func_num_primes, x, num_primes)
+# print('Curve Parameter (a,b) = ',param)
 ans = x/np.log(param[0]*x+param[1]) 
-  
 
-plt.plot(x, y,'-', color ='blue', label ="Actual") 
-plt.plot(x, log_values, '-', color ='red', label ="x/ln(x)")
+fig = plt.figure(figsize=(8, 6))
+plt.subplot(2,1,1)
+plt.plot(x, num_primes,'-', color ='blue', label ="Actual #Primes") 
+plt.plot(x, log_values, '-', color ='red', label ="N/ln(N)")
 plt.plot(x, ans, '-', color ='green', label ="Optimized")
+plt.title('Number of Primes')
 plt.xlabel('N')
 plt.ylabel('#Primes')
-plt.legend()
+plt.legend(loc="best")
+
+prime_density = []
+for i in range(0,len(num_primes)):
+    prime_density.append(num_primes[i]/x[i])
+
+density_log_values = []
+for i in x:
+    density_log_values.append(1/np.log(i))
+
+def test_func_prime_density(x, a, b):
+    return 1/np.log(x*a+b) 
+
+param, param_cov = optimize.curve_fit(test_func_prime_density, x, prime_density)
+# print('Curve Parameter (a,b) = ',param)
+ans = 1/np.log(param[0]*x+param[1]) 
+
+
+plt.subplot(2,1,2)
+plt.plot(x, prime_density,'-', color ='blue', label ="Actual Density") 
+plt.plot(x, density_log_values, '-', color ='red', label ="1/ln(N)")
+plt.plot(x, ans, '-', color ='green', label ="Optimized")
+plt.title('Prime Density')
+plt.xlabel('N')
+plt.ylabel('Density')
+plt.legend(loc="best")
+
+plt.tight_layout()
 plt.show()
